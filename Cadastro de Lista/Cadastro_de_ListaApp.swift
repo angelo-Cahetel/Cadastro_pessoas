687f2586bcce8e5af28cd3ff3c68932776776cf7
Angelo Mendes <angelo030825@gmail.com>
2025-04-15 19:14:25 -0300

    Initial Commit

---
+++ Cadastro de Lista/Cadastro_de_ListaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cadastro_de_ListaApp.swift
+//  Cadastro de Lista
+//
+//  Created by Ângelo Mendes on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Cadastro_de_ListaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cadastro de Lista/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cadastro de Lista
+//
+//  Created by Ângelo Mendes on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
